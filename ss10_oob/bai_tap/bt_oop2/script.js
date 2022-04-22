ba1b612c4612584840e8c0a2eade52acc1e1f474
thlq14 <ntlonq14@gmail.com>
2022-04-22 08:01:51 +0700

    up bt oop

--- ss10_oob/bai_tap/bt_oop2/script.js
+++ ss10_oob/bai_tap/bt_oop2/script.js
@@ -1,30 +1,31 @@
-
-function Hero(image, top, left, size){
-    this.image = image;
-    this.top = top;
-    this.left = left;
-    this.size = size;
-
-    this.getHeroElement = function(){
-        return '<img width="'+ this.size + '"' +
-            ' height="'+ this.size + '"' +
-            ' src="' + this.image +'"' +
-            ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+class Hero {
+    constructor(image, top, left, size) {
+        this.image = image;
+        this.top = top;
+        this.left = left;
+        this.size = size;
     }
 
-    this.moveRight = function(){
+    getHeroElement() {
+        return '<img width="' + this.size + '"' +
+            ' height="' + this.size + '"' +
+            ' src="' + this.image + '"' +
+            ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
+    }
+
+    moveRight() {
         this.left += 20;
         console.log('ok: ' + this.left);
     }
-
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+let hero = new Hero('pokemon.png', 20, 30, 200);
 
-function start(){
-    if(hero.left < window.innerWidth - hero.size){
+function start() {
+    if (hero.left < window.innerWidth - hero.size) {
         hero.moveRight();
     }
+
     document.getElementById('game').innerHTML = hero.getHeroElement();
     setTimeout(start, 500)
 }
